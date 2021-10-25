0f6001579add52c9a99db0427a6aeecd16968686
Peggy Lindner <plindner@uh.edu>
2021-10-25 17:05:15 -0500

    setup configuration files to use .env

--- config/database.js
+++ config/database.js
@@ -4,16 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
+        host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+        srv: env.bool('DATABASE_SRV', process.env.DATABASE_SRV),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'Syrios'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+        username: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+        password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', process.env.AUTHENTICATION_DATABASE),
+        ssl: env.bool('DATABASE_SSL', process.env.DATABASE_SSL),
       },
     },
   },
--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '8885f6fa177f2dabe6aaf15bff3fec65'),
+      secret: env('ADMIN_JWT_SECRET', process.env.ADMIN_JWT_SECRET),
     },
   },
 });
