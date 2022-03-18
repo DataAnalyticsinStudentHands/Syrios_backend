4a0b735efa0cdc14965c021e0bbcb458a0ff59c9
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-03-18 08:54:38 -0500

    Trying the new email template

--- api/download/controllers/download.js
+++ api/download/controllers/download.js
@@ -1,25 +1,53 @@
 'use strict'
 
-module.exports = {
-  async send(ctx) {
-    const body = ctx.request.body
-    const sendTo = body.email
-    const fullName = body.fullName
-    strapi.log.debug(`Trying to send an email to ${sendTo} and his name is ${fullName}`)
-
-    try{
-      await strapi.plugins['email'].services.email.send({
-          to:sendTo,
-          from:"shaotianhao1997@gmail.com",
-          // replayTo:"shaotianhao1997@gmail.com",
-          subject:"Test nodemailer and Strapi", 
-          text:`${fullName} request download data, and the email is ${sendTo}`
-      });
-      ctx.send("Email sent!");
-    } 
-    catch (err) {
-      strapi.log.error(`Error sending email to ${sendTo}`, err)
-      ctx.send({ error: 'Error sending email' })
-    }
+const nodemailer = require("nodemailer");
+let transporter = nodemailer.createTransport({
+  sendmail: true,
+  newline: 'unix',
+  path: '/usr/sbin/sendmail'
+});
+module.exports={
+  async send(ctx){
+    transporter.sendmail({
+      from: "Syrios Site Watcher <dashadmin@uh.edu>",
+      to: 'shaotianhao1997@gmail.com',
+      subject: 'Message',
+      text: 'I hope this message gets delivered!'
+  }, (err, info) => {
+      console.log(info.envelope);
+      console.log(info.messageId);
+  })
   }
 }
+
+// module.exports = {
+//   async send(ctx) {
+//     const body = ctx.request.body
+//     const sendTo = body.email
+//     const fullName = body.fullName
+//     strapi.log.debug(`Trying to send an email to ${sendTo} and his name is ${fullName}`)
+
+//     try{
+//       await strapi.plugins['email'].services.email.send({
+//           to:sendTo,
+//           from:"shaotianhao1997@gmail.com",
+//           // replayTo:"shaotianhao1997@gmail.com",
+//           subject:"Test nodemailer and Strapi", 
+//           text:`${fullName} request download data, and the email is ${sendTo}`
+//       });
+//       ctx.send("Email sent!");
+//     } 
+//     catch (err) {
+//       strapi.log.error(`Error sending email to ${sendTo}`, err)
+//       ctx.send({ error: 'Error sending email' })
+//     }
+//   }
+// }
+
+// module.exports={
+//   let transporter = nodemailer.createTransport({
+//     sendmail: true,
+//     newline: 'unix',
+//     path: '/usr/sbin/sendmail'
+// });
+// }
--- config/plugins.js
+++ config/plugins.js
@@ -7,3 +7,21 @@
       },
     },
   });
+
+  // module.exports = ({ env }) => ({
+  //   email: {
+  //       provider: 'nodemailer',
+  //       providerOptions: {
+  //         host: 'localhost',
+  //         port: 1025,
+  //         auth: {
+  //           user: 'project.1',
+  //           pass: 'secret.1',
+  //         },
+  //       },
+  //       settings: {
+  //         defaultFrom: 'shaotianhao1997@gmail.com',
+  //         defaultReplyTo: 'shaotianhao1997@gmail.com',
+  //       },
+  //   },
+  // });
