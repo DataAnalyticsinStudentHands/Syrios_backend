b1031176a29515d8e6222fd54ef6f25ec4af5e61
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-03-18 10:40:05 -0500

    Clean up the download email file

--- api/download/controllers/download.js
+++ api/download/controllers/download.js
@@ -20,26 +20,3 @@ module.exports = {
   },
 };
 
-// module.exports = {
-//   async send(ctx) {
-//     const body = ctx.request.body
-//     const sendTo = body.email
-//     const fullName = body.fullName
-//     strapi.log.debug(`Trying to send an email to ${sendTo} and his name is ${fullName}`)
-
-//     try{
-//       await strapi.plugins['email'].services.email.send({
-//           to:sendTo,
-//           from:"shaotianhao1997@gmail.com",
-//           // replayTo:"shaotianhao1997@gmail.com",
-//           subject:"Test nodemailer and Strapi", 
-//           text:`${fullName} request download data, and the email is ${sendTo}`
-//       });
-//       ctx.send("Email sent!");
-//     } 
-//     catch (err) {
-//       strapi.log.error(`Error sending email to ${sendTo}`, err)
-//       ctx.send({ error: 'Error sending email' })
-//     }
-//   }
-// }
