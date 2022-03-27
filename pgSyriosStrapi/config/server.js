276e733259b25d5fe8ad25403bf7804ea888a363
Shane Whitmire <dogunbound5@gmail.com>
2022-03-27 18:03:23 -0500

    made a mistake

--- pgSyriosStrapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'c823d00b203da07a5fd10c76d2ee223a'),
-    },
-  },
-});
--- pgSyriosStrapi/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '74f4b06c-76e9-4764-b4a3-ca343403ea72'
-};
