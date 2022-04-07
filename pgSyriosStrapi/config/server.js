1698b450b5828709ad61829cdebba673e34cebb0
Shane Whitmire <dogunbound5@gmail.com>
2022-04-07 14:34:44 -0500

    removed everything

--- pgSyriosStrapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '8628df9bc0d3cfd2811e8f350646028d'),
-    },
-  },
-});
--- pgSyriosStrapi/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '5a028e5a-baae-4a58-ac1f-71c98f6cb223'
-};
