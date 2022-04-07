c477bcf4f2490ac8b26c012b62787f3ddadb92c5
Shane Whitmire <dogunbound5@gmail.com>
2022-04-07 14:48:12 -0500

    init pg strapi 3.x.x

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a1ff5225517a17c01036e3b81a5f14e3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd905e51c-e28d-40eb-a1c6-78e353cd1662'
+};
