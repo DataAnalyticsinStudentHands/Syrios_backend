6fdf7a34b462307ec1d2a28c7554a22095b18c5e
Shane Tran Whitmire <dogunbound5@gmail.com>
2022-06-24 10:19:09 -0500

    Added tool tips to coin sort

--- src/api/coin-sort/controllers/coin-sort.js
+++ src/api/coin-sort/controllers/coin-sort.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::coin-sort.coin-sort');
+module.exports = createCoreController('api::coin-sort.coin-sort', ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = {
+      populate: [
+        'arrangement_tips',
+        'sorting_tips',
+        'filtering_tips',
+      ],
+      ...ctx.query
+    };
+
+    const { data, meta } = await super.find(ctx);
+
+    return { data, meta };
+  }
+}));
