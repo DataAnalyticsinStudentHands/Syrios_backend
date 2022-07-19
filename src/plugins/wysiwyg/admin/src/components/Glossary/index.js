62ddd95f53e2179dd7023a527a9c58b01c9f39a4
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-07-19 09:29:30 -0500

    Sort the Glossary term

--- src/plugins/wysiwyg/admin/src/components/Glossary/index.js
+++ src/plugins/wysiwyg/admin/src/components/Glossary/index.js
@@ -1,14 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Stack } from '@strapi/design-system/Stack';
-import { Typography } from '@strapi/design-system/Typography';
-import { Radio, RadioGroup } from '@strapi/design-system/Radio';
 import { Button } from '@strapi/design-system/Button';
-import { Box } from '@strapi/design-system/Box';
 import wysiwygRequests from '../../api/wysiwug';
 
 import { Select, Option } from '@strapi/design-system/Select';
-import { Grid, GridItem } from '@strapi/design-system/Grid';
 const Glossary = ({
     disabled,
     editorRef,
@@ -20,30 +16,33 @@ const Glossary = ({
 
     const handleInsert = ()=>{insertReference(editorRef)}
     const insertReference = (editor) => {
-        let referenceContent=`<a href="/dev/Toolbox/Glossary/term/${value}" class="glossary-tag">${value}<i class='story-icon'> &#xe817;</i></a>`
+        let referenceContent=`<a href="/dev/Toolbox/Glossary/term/${value}" class="glossary-tag">${value}<sup class='story-icon'> &#xe817;</sup></a>`
         editor.current.insertContent(referenceContent)
         setTimeout(() => editor.current.focus(), 0);
         setValue("")
       };
-    
-    async function fetchGlossary(){
-        const result = await wysiwygRequests.findCollectionTypes('glossary')
-        setGlossaryData(result.results)
-    }
 
+    async function fetchGlossary(){
+        let result = await wysiwygRequests.findCollectionTypes('glossary')
+        function compare(a,b){
+            if ( a.term < b.term  ){return -1;}
+            if ( a.term  > b.term  ){return 1;}
+            return 0;}
+        result = result.results.sort(compare)
+        setGlossaryData(result)
+    }
 
     return(
         <>
             <Stack horizontal spacing={3} padding={3}>
                 <Button variant='secondary' onClick={()=>{fetchGlossary()}}>Glossary</Button>
-                {value.length === 0 ? (<></>):(<Button size="S" onClick={()=>{handleInsert()}}>Inster into Editor</Button>)}
+                {value.length === 0 ? (<></>):(<Button size="S" onClick={()=>{handleInsert()}}>Instert</Button>)}
             </Stack>
             
             {glossaryData.length === 0 ? (<></>):(
                 <Select id="select1"
                     placeholder="Select Glossary term"
-                    onClear={() => setValue("")} clearLabel="Clear the meal" 
-                    // error={error} 
+                    onClear={() => setValue("")}
                     value={value} 
                     onChange={setValue}>
                     {glossaryData.map((term)=>{return(<Option value={term.term} key={term.id}>{term.term}</Option>)})}
@@ -62,6 +61,5 @@ Glossary.propTypes={
     editorRef: PropTypes.shape({ current: PropTypes.any }).isRequired,
     name: PropTypes.string.isRequired,
     value: PropTypes.string,
-
 }
 export default Glossary
