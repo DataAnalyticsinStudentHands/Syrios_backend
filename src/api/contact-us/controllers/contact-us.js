ef7c2c8b3d33ca0670ba154e905766875af02c1a
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-07-21 09:27:45 -0500

    Update on Contact-Email

--- src/api/contact-us/controllers/contact-us.js
+++ src/api/contact-us/controllers/contact-us.js
@@ -20,13 +20,24 @@ module.exports = createCoreController('api::contact-us.contact-us', ({ strapi })
     return { data, meta };
   },
 
-  async send(ctx) {    
-    const emailTo = ctx.request.body.emailTo
-    const emailSubject = ctx.request.body.emailSubject
+  async send(ctx) {   
+    const result = await strapi.entityService.findPage('api::contact-us.contact-us')
+    const emailTo = result['results'][0]['emailTo']
+    const emailSubject = result['results'][0]['emailSubject']
+
     const name =ctx.request.body.name    
     const email =ctx.request.body.email    
     const phone =ctx.request.body.phone    
     const message =ctx.request.body.writtenMessage
+
+    strapi.db.query('api::contact-user-info.contact-user-info').create({
+      data: {
+        name: name,
+        email:email,
+        phone:phone,
+        message:message,
+      },
+    });
 
     strapi.service('api::download.download').send(
       email,       
