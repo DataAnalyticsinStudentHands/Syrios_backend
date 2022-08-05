2c129e8dede7b52ddf3f13f456360f18da502276
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-08-05 08:18:30 -0400

    Update on event controllers

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -6,4 +6,31 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController('api::event.event', ({strapi})=>({
+    async find(ctx) {
+        ctx.query = {
+          populate: [
+            'tag_subcategory1',
+            'tag_subcategory2'
+          ],
+          pagination: {
+            pageSize: 100,
+          },
+          ...ctx.query
+        };
+    
+        const { data, meta } = await super.find(ctx);
+        return { data, meta };
+      },
+      async findOne(ctx) {
+        ctx.query = {
+          populate: [
+            'tag_subcategory1',
+            'tag_subcategory2'
+          ],
+              ...ctx.query
+        };
+        const { data, meta } = await super.findOne(ctx);
+        return { data, meta };
+      }
+}));
