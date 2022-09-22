32084fec27db693b1d6007dc0a7e5a94e23c198e
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2022-09-22 08:28:45 -0500

    landing page - Hardcode replace

--- src/api/landing-page/controllers/landing-page.js
+++ src/api/landing-page/controllers/landing-page.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::landing-page.landing-page');
+module.exports = createCoreController('api::landing-page.landing-page', ({ strapi }) => ({
+    async find(ctx) {
+      ctx.query = {
+        populate: [
+          'image_icons',
+          'image_icons.image',
+        ],
+        ...ctx.query
+      };
+  
+      const { data, meta } = await super.find(ctx);
+  
+      return { data, meta };
+    }
+  }));
