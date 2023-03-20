dc26a42bb454f6c28630214a9e0a4f10914c6203
Tianhao-Shao <70172849+Tianhao-Shao@users.noreply.github.com>
2023-03-20 16:48:41 -0500

    Update Strapi Version

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'c44f05d69da15b572cd085786142ee0e'),
+    secret: env('ADMIN_JWT_SECRET', process.env.ADMIN_JWT_SECRET),
   },
+  apiToken:{ 
+      salt: env('API_TOKEN_SALT', process.env.API_TOKEN_SALT),
+  }
 });
--- config/database.js
+++ config/database.js
@@ -2,12 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', process.env.DATABASE_HOST),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'syrios'),
-      user: env('DATABASE_USERNAME', ''),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+      user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+      password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
     },
   },
 });
